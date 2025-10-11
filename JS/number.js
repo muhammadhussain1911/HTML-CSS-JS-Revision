1f09945b1b5c2b392f75ca2add5286bb42907124
Muhammad Hussain <ayesq0003@gmail.com>
2025-10-11 21:54:02 +0500

    JS numbers concept in detail created.

---
+++ JS/number.js
@@ -0,0 +1,44 @@
+// const age = 42; // JS automatically recognize it a number. No 100% assurity that it would be a number in case data is fetched from server
+// console.log(age);
+// console.log( typeof age); // number
+
+// const price = new Number(100); // There is 100% assurity that it is a number.
+// console.log(price);
+// console.log(typeof price); // object
+
+// Number Methods:
+
+// const price1 = 899.56;
+// let preciseNumber = price1.toPrecision(4); // It takes a number that tell how much the length of returning value, and returns a string.
+// console.log(preciseNumber);
+
+// const number = 4567.765;
+// const fixedNumber = number.toFixed(2);
+// console.log(fixedNumber);
+
+
+// const oneMillion = 1000000;
+// console.log(oneMillion.toLocaleString());
+// console.log(oneMillion.toLocaleString("en-IN"));
+// console.log(oneMillion.toExponential(5));
+
+// const number1 = 14749547;
+// const scientificNotation = number1.toExponential(4);
+// console.log(scientificNotation);
+
+
+
+
+// Number Methods
+
+console.log(Number.EPSILON);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+
+console.log(Number.NaN);
+// console.log(Number.arguments);
+console.log(Number.NaN);
