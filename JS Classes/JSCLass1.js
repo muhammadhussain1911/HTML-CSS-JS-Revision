26ee3de6f92b0aded6ecb54df9a280c087310eaf
Muhammad Hussain <ayesq0003@gmail.com>
2025-12-18 16:24:45 +0500

    JS first lectur delivered

---
+++ JS Classes/JSCLass1.js
@@ -0,0 +1,38 @@
+// let myName = "Moazzam";
+//   console.log(myName);
+
+// var age = 12;
+// console.log("myName");
+// var myName = "Abdul Sami";
+// var myName = "Moazzam";
+// console.log(myName);
+
+// let myName = "Abdul Sami";
+// let myName = "Moazzam";
+// console.log(myName);
+
+// let names; // declaration
+// names = "Hussain"; // initialization
+
+ /* console.log("j");
+console.log("j");
+console.log("j");
+console.log("j");
+console.log("j");
+console.log("j");*/
+
+// var age = 23;
+// var age = 24;
+// console.log(age);
+
+// let klass = "10th";
+// klass = "11th";
+// let klass = "12th";
+// console.log(klass);
+
+const price = 1000;
+const Price = 10000;
+// price = 10000;
+console.log(Price); 
+
+var myCurrentAge
